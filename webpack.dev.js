ef23d69148061fb166720aa7c2b6d7d65d4b2163
SinonXZH <150547394+SinonXZH@users.noreply.github.com>
2026-03-27 12:12:40 +0800

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+// webpack.dev.js
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+  mode: "development",
+
+  devtool: "eval-source-map",
+
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    open: true,
+  },
+});
---
+++ webpack.prod.js
@@ -0,0 +1,9 @@
+// webpack.prod.js
+import { merge } from "webpack-merge";
+import common from "./webpack.common";
+
+export default merge(common, {
+  mode: "production",
+
+  devtool: "source-map",
+});
